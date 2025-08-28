d58b552c1d6b5d5a912d586fbb71256e7f55a794
zhangye.111000 <zhangye.111000@bytedance.com>
2025-08-28 20:10:43 +0800

    add package .el
    Change-Id: I11f68161f70bb875d93b9031c30591d457d58880

---
+++ org-tags-outline.el
@@ -0,0 +1,14 @@
+;;; org-tags-outline.el --- Tags outline in tree view. -*- lexical-binding: t; -*-
+
+;; Copyright © 2025-2025 Ye Zhang <yezhang989@gmail.com>
+
+;; Author: Ye Zhang <yezhang989@gmail.com>
+;; URL: https://github.com/yezhang/org-tags-outline
+;; Keywords: org-mode, org-tags, tags, outline
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "29.4"))
+
+
+
+
+;;; org-tags-outline.el ends here
